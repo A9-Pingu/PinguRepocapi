4722fa7d2ddafc84811de400f51632321d33266e
Euronia12 <lkh0647@naver.com>
2024-04-29 18:18:19 +0900

    초기화입니다.
    초기화

--- ConsoleGame/obj/Debug/net8.0/ConsoleGame.AssemblyInfo.cs
+++ ConsoleGame/obj/Debug/net8.0/ConsoleGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c15e513d82f07cdb81b7a5fdde690a70c8fb2a37")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
