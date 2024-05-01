f7c4d79725d38eb4100f3538173ce9b13950249b
Euronia12 <lkh0647@naver.com>
2024-05-01 14:26:51 +0900

    [Feat]Create additional tem
    기획했던 아이템들을 객체로 생성하여 ItemInfos에 추가하였습니다.

--- ConsoleGame/Managers/ItemManager.cs
+++ ConsoleGame/Managers/ItemManager.cs
@@ -6,17 +6,32 @@ namespace ConsoleGame.Managers
     {
         public List<Item> ItemInfos = new List<Item>
         {
-            new Item("수련자 갑옷", ItemType.Armor, 1000, 5, "수련에 도움을 주는 갑옷입니다."),
+            new Item("천 갑옷", ItemType.Armor, 1000, 5, "수련에 도움을 주는 갑옷입니다."),
             new Item("무쇠갑옷", ItemType.Armor, 2000, 9, "무쇠로 만들어져 튼튼한 갑옷입니다."),
             new Item("스파르타의 갑옷", ItemType.Armor, 3500, 15, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다."),
             new Item("헤라클레스의 망토", ItemType.Armor, 5000, 30, "헤라클레스가 헤르메스에게 받았다는 전설의 망토입니다."),
-            new Item("낡은 검", ItemType.Weapon, 600, 2, "쉽게 볼 수 있는 낡은 검입니다."),
+            new Item("도란의 검", ItemType.Weapon, 600, 2, "쉽게 볼 수 있는 낡은 검입니다."),
             new Item("청동 도끼", ItemType.Weapon, 1400, 5, "어디선가 사용됐던거 같은 도끼입니다."),
             new Item("스파르타의 창", ItemType.Weapon, 2500, 7, "스파르타의 전사들이 사용했다는 전설의 창입니다."),
-            new Item("여명의 시미터", ItemType.Weapon, 3500, 20, "달의 여신의 축복을 받아 만들어진 검입니다."),
+            new Item("헤르메스의 시미터", ItemType.Weapon, 3500, 20, "달의 여신의 축복을 받아 만들어진 검입니다."),
             new Item("체력 회복 물약", ItemType.Consumable, 100, 0, "체력을 50 회복시키는 물약입니다."),
             new Item("마나 회복 물약", ItemType.Consumable, 100, 0, "마나를 30 회복시키는 물약입니다."),
-            new Item("공격력 증가 물약", ItemType.Consumable, 200, 0, "공격력을 5 증가시키는 물약입니다.")
+            new Item("공격력 증가 물약", ItemType.Consumable, 200, 0, "공격력을 5 증가시키는 물약입니다."),
+            new Item("펭귄 성장용 깃털", ItemType.Armor, 900, 5, "특별 제작된 펭모로 늠름함을 높입니다."),
+            new Item("북극곰의 투구", ItemType.Armor, 1200, 10, "털갈이를 하는 북극곰의 옆에서 채취해 만든 투구입니다."),
+            new Item("순록의 털옷", ItemType.Armor, 2100, 15, "북극에서 천적에 의해 사냥당한 순록의 털로 만든 옷입니다."),
+            new Item("방문객의 연구복", ItemType.Armor, 3000, 20, "이곳을 연구하는 연구원이 마지막으로 남기고 간 옷입니다."),
+            new Item("조개 껍데기", ItemType.Weapon, 1000, 5, "물개 아저씨의 비상식량이었습니다. 쓸만합니다."),
+            new Item("말린 연어", ItemType.Weapon, 1500, 15, "연어를 햇볕에 말려 딱딱해졌습니다. 제법 단단합니다."),
+            new Item("참치 대가리", ItemType.Weapon, 2500, 25, "어린 참치의 동족으로 만들어진 검입니다. 묵직합니다!"),
+            new Item("냉동 상어", ItemType.Weapon, 3500, 35, "영하 30도의 온도로 꽝꽝 얼려진 냉동 대검입니다!"),
+            new Item("알", ItemType.Consumable, 1000,5, "폭탄은 예술입니다."),                               //임시
+            new Item("눈덩이", ItemType.Consumable, 1500, 10, "무언가를 눈으로 감싸만든 눈덩이입니다."),     //임시
+            new Item("고드름", ItemType.Consumable, 2400, 20, "가시와 동급이거나 그 이상입니다."),           //임시
+            new Item("뼈다귀 부메랑", ItemType.Consumable, 3000, 30, "사랑은 돌아오는 거야!"),               //임시
+            new Item("새우", ItemType.Consumable, 200, 20, "맛있습니다! 체력을 20 회복하는 음식입니다."),
+            new Item("조개살 죽", ItemType.Consumable, 200, 20, "물개 아저씨의 야심작입니다! 마나를 20 회복하는 음식입니다."),
+            new Item("해파리", ItemType.Consumable, 1000, 5, "먹으면 따끔합니다! 공격력이 5 증가하는 음식입니다."),
         };
 
         public List<Item> specialItems = new List<Item>
