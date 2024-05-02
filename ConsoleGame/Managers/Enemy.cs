d7ef7b5842868545b590185847418e6f42dd0ac8
Lee-hyun2 <167046397+Lee-hyun2@users.noreply.github.com>
2024-05-02 17:41:04 +0900

    dd

--- ConsoleGame/Managers/Enemy.cs
+++ ConsoleGame/Managers/Enemy.cs
@@ -23,13 +23,32 @@ namespace ConsoleGame.Managers
         }
         public void EnemyAttack(Character player)
         {
+<<<<<<< Updated upstream
             int damage = AttackPower - player.CalculateTotalDefensePower();
             if (damage < 0)
+=======
+            // 회피할 확률을 확인합니다.
+            Random random = new Random();
+            // 10% 확률로 회피 발생
+            bool isDodged = random.Next(1, 101) <= 10;
+
+            // 회피가 발생한 경우
+            if (isDodged && Name != "스킬") // 스킬은 회피할 수 없음
+>>>>>>> Stashed changes
             {
-                damage = 0;
+                Console.WriteLine($"{Name}이(가) 당신의 공격을 회피했습니다.");
             }
-            Console.WriteLine($"{Name}이(가) 당신에게 {damage}의 피해를 입혔습니다.");
-            player.Health -= damage;
+            else
+            {
+                // 회피가 발생하지 않은 경우에만 공격을 수행합니다.
+                int damage = AttackPower;
+                if (damage < 0)
+                {
+                    damage = 0;
+                }
+                Console.WriteLine($"{Name}이(가) 당신에게 {damage}의 피해를 입혔습니다.");
+                player.Health -= damage;
+            }
         }
     }
 }
