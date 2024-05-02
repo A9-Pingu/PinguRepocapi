683458fe70e32951c496608ff45c4b8e20d97558
Lee-hyun2 <167046397+Lee-hyun2@users.noreply.github.com>
2024-05-02 23:28:08 +0900

    Revert "Merge pull request #6 from shinmegan/dev"

--- ConsoleGame/Managers/Enemy.cs
+++ ConsoleGame/Managers/Enemy.cs
@@ -12,17 +12,88 @@ namespace ConsoleGame.Managers
         public string Name { get; set; }
         public int Level { get; set; }
         public int Health { get; set; }
-        public int AttackPower { get; set; }
+        public int Attack { get; set; }
 
-        public Enemy(int level, int health, int attackPower, string name)
+        public bool isDead { get; set; }
+
+        public Enemy(string name, int level, bool isDead = false)
         {
             Level = level;
-            Health = health;
-            AttackPower = attackPower;
             Name = name;
+            Health = CalculateHealth(level); //레벨반영
+            Attack = CalculateAttack(level);
+            this.isDead = isDead;
+        }
+
+        //플레이어 레벨 반영하여 몬스터 체력 조정
+        private int CalculateHealth(int level)
+        {
+            switch (Name)
+            {
+                case "도둑갈매기":
+                    return 30 + level * 4;
+                case "야생들개":
+                    return 40 + level * 4;
+                case "여우":
+                    return 45 + level * 4;
+                case "바다표범":
+                    return 80 + level * 5;
+                case "늑대":
+                    return 60 + level * 5;
+                case "북극곰":
+                    return 100 + level * 6;
+                case "범고래":
+                    return 150 + level * 6;
+                default:
+                    return 0;
+            }
+        }
+
+        //플레이어 레벨 반영하여 몬스터 공격력 조정
+        private int CalculateAttack(int level)
+        {
+            switch (Name)
+            {
+                case "도둑갈매기":
+                    return 4 + level;
+                case "야생들개":
+                    return 6 + level;
+                case "여우":
+                    return 6 + level;
+                case "바다표범":
+                    return 10 + level * 2;
+                case "늑대":
+                    return 14 + level * 2;
+                case "북극곰":
+                    return 20 + level * 3;
+                case "범고래":
+                    return 17 + level * 3;
+
+                default:
+                    return 0;
+            }
         }
         public void EnemyAttack(Character player)
         {
+
+            if (player.Health < Attack)
+            {
+                player.Health = 0;
+            }
+            else
+                player.Health -= Attack;
+            Console.WriteLine("===================");
+            Console.WriteLine($"{Name} 의 공격!");
+            Console.WriteLine($"Lv.{player.Level} {player.Name} 에게 {Attack} 데미지를 가했습니다.");
+            Console.WriteLine($"");
+            Console.WriteLine($"Lv.{player.Level} {player.Name}");
+            Console.WriteLine($"HP {player.MaxHealth} -> {player.Health}");
+            Console.WriteLine($"");
+            Console.WriteLine($"0. 다음");
+            Console.WriteLine($"1. 아이템사용");
+            Console.WriteLine($"");
+            Console.Write(">>");
+
             // 회피할 확률을 확인합니다.
             Random random = new Random();
             bool isDodged = random.Next(1, 101) <= 10; // 10% 확률로 회피 발생
@@ -43,6 +114,7 @@ namespace ConsoleGame.Managers
                 Console.WriteLine($"{Name}이(가) 당신에게 {damage}의 피해를 입혔습니다.");
                 player.Health -= damage;
             }
+
         }
 
     }
